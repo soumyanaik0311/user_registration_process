340dc0de83cbcdf9ee761c3486b88b1e2fd6e0a8
Soumya Ranjan Naik <75331590+soumyanaik0311@users.noreply.github.com>
2024-08-23 14:08:34 +0530

    Revert "User logout"

--- user_reg/urls.py
+++ user_reg/urls.py
@@ -23,10 +23,6 @@ from app.views import *
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("registration/",registration,name='registration'),
-    path("home/",home,name='home'),
-    path("user_login/",user_login,name='user_login'),
-    path("user_logout/",user_logout,name='user_logout'),
-    path("display_data/",display_data,name='display_data'),
 
 
 
